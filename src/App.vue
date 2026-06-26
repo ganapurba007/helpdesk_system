efedd9d3887b04fe1bab9e012d2c8bce56bf2bfb
ganapurba007 <ganapurbakusuma86@gmail.com>
2026-06-26 09:40:31 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Nav from "./components/Nav.vue";
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <Nav />
+
+  <main class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-20">
+    <RouterView />
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/HomeView.vue";
+import About from "../views/AboutView.vue";
+import Login from "../views/LoginView.vue";
+import Error from "../views/404View.vue";
+
+const route = [
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: About,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
+  { path: "/:catchAll(.*)", component: Error },
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: route,
+});
+
+export default router;
